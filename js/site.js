6b07dd9029d2371a10fb23ca80216b07a773e8d7
Ryan Penley <ryanpenley@gmail.com>
2023-01-18 14:53:38 -0500

    wrote reverse string js

--- js/site.js
+++ js/site.js
@@ -1,12 +1,26 @@
-function displayMessage() {
-    let msg = document.getElementById('message').value;
+// controller
+function getValues() {
+    let inputString = document.getElementById('userString').value;
 
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'U-Turn',
-            text: msg
-        }
-    )
-    
+    let reversedString = reverseAString(inputString);
+
+    displayString(reversedString);
+}
+
+// logic function
+// reverse a string
+function reverseAString(userString) {
+    let revString = '';
+
+    for(let i = userString.length - 1; i >= 0; i = i - 1) {
+        revString += userString[i];
+    }
+
+    return revString;
+}
+
+// view function
+function displayString(revString) {
+    document.getElementById('results').textContent = revString;
+    document.getElementById('alert').classList.remove('invisible');
 }
